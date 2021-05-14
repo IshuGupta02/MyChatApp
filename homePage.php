1c1e1305bf8f734f2d5eed0e0be3a3c94c8f7d8d
root <ishu_g@cs.iit.ac.in>
2021-05-14 12:46:20 +0530

    Create some files and ADD basic html structure

---
+++ homePage.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>
+        WELCOME TO MyChat!
+    </h1>
+
+
+    <div>
+
+        <div class="signup">
+            <h2> SIGNUP </h2>
+            <form action="">
+                <label for=""> YOUR NAME</label>
+                <input type="text" name="" id="">
+
+                <label for="">USERNAME</label>
+                <input type="text" name="" id="">
+
+                <label for="">PASSWORD</label>
+                <input type="password" name="" id="">
+
+                <label for="">CONFIRM PASSWORD</label>
+                <input type="password" name="" id="">
+
+                <button>CREATE ACCOUNT</button>
+                
+
+
+            </form>
+
+
+        </div>
+
+        <div class="login">
+            <h2> LOGIN </h2>
+            <form action="">
+                <label for="">USERNAME</label>
+                <input type="text" name="" id="">
+
+                <label for="">PASSWORD</label>
+                <input type="password" name="" id="">
+
+                <input type="checkbox" name="" id="">
+                <label for="">Remember Me</label>
+
+                <button>LOGIN</button>
+
+            </form>
+
+        </div>
+
+
+    </div>
+    
+</body>
+</html>
