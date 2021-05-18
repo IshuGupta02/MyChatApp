de83e9c3ddfbcf3f03d6589a592db74bb0c3d187
root <ishu_g@cs.iit.ac.in>
2021-05-18 12:39:03 +0530

    Add show all users option

--- chatroom.php
+++ chatroom.php
@@ -5,27 +5,108 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="chatroomcss.css">
     
 
 </head>
-<body onload="checkprofile()">
+<body >
 
-    <form action="logout.php" method="post">
-        <button id="logout">LOGOUT</button>
-    
-    </form>
+    <div class="otheroptions">
+        <form action="logout.php" method="post" class="redirect">
+            <button id="logout" class="goto">LOGOUT</button>
+        
+        </form>
 
-    <form action="profile.php" method="post">
-        <button id="profile">GO TO PROFILE PAGE!</button>
-    
-    </form>
+        <form action="profile.php" method="post" class="redirect">
+            <button id="profile" class="goto">GO TO PROFILE PAGE!</button>
+        
+        </form>
 
-    <!-- <button id="profile">GO TO PROFILE PAGE!</button> -->
 
-    <?php
+    </div>
+
+    <div class="chat">
+        <?php
+            require_once 'connect.php';
+
+            $conn = new mysqli($servername, $username, $password, $db);
+
+            //Check connection
+            if ($conn->connect_error) {
+                echo "<div class=\"output\">SORRY! COULD NOT LOAD CHATROOM!</div>";
+                die;
+            }
+
+            $username_name="username";
+            $password_name="password";
+            $remember_name="remember";
+
+            $username=$_COOKIE[$username_name];
+            $pass=$_COOKIE[$password_name];
+            $rem=$_COOKIE[$remember_name];
+
+            $sql="SELECT * from users where Username=\"".$username."\""." AND ". "Password=\"".$pass."\"";
+            
+            $result=$conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+
+                    $profile=$row['profile'];
+                    $req="yes";
+                
+                    if($profile!==$req){
+                        echo "<div class=\"output\">YOUR PROFILE IS NOT COMPLETED. PLEASE COMPLETE YOUR PROFILE FIRST.</div>";
+                        die;
+
+                    }
+                    else{
+                        echo "<div class=\"output\">WELCOME TO YOUR CHATROOM!</div>";
+
+
+                    }
+
+                }
+            }
+            else{
+                echo "<div class=\"output\">SORRY! COULD NOT LOAD CHATROOM!</div>";
+                die;
+            }
+
+            echo "<div class='allusers'>";
+
+
+            $sql1="SELECT * from users";
+            $result1=$conn->query($sql1);
+
+            if ($result1->num_rows > 0) {
+                // echo "entered1";
+                // output data of each row
+                while($row1 = $result1->fetch_assoc()) {
+                    // echo "entered";
+
+                    echo "<div class='users'>";
+                    echo $row1['Username'];
+                    echo "</div>";
+                
+                }
+            }
+            else{
+                echo "<div class=\"users\">NO USERS FOUND</div>";
+                die;
+            }
+
+            echo "</div>";
+
+        
+        ?>
+
+
+    </div>
+
     
-    
-    ?>
     
 </body>
 <script scr="chatroomscript.js"></script>
